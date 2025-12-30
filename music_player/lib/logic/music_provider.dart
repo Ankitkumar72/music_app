015f8074dafda154aa5cc7ab9d385954c78c5a1c
Ankit Kumar <79219070+Ankitkumar72@users.noreply.github.com>
2025-12-30 20:35:10 +0530

    Fix import

--- music_player/lib/logic/music_provider.dart
+++ music_player/lib/logic/music_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:hive/hive.dart';
-import 'models/song_data.dart'; // Ensure this path is correct for your project
+import 'models/song_data.dart';
 
 class MusicProvider extends ChangeNotifier {
   final OnAudioQuery _audioQuery = OnAudioQuery();
